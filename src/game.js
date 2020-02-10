da7690b25d4eab732bd61b44542a780d3c7b24cd
gardoroman <gardoroman@gmail.com>
2020-02-09 18:54:39 -0600

    Implement time travel.

--- src/game.js
+++ src/game.js
@@ -11,14 +11,15 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null)
       }],
-      xIsNext: true
+      xIsNext: true,
+      stepNumber: 0
     };
   }
 
   handleClick(i) {
-    console.log("hi")
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
+    
     const squares = current.squares.slice();
 
     // The squares[i] prevents a previously played
@@ -31,17 +32,33 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
     })
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
   }
 
   render() {
     const history = this.state.history;
-    console.log('first history', history)
-
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
-    console.log('first winner', winner)
+
+    const moves = history.map((_step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)} > {desc} </button>
+          </li>
+        )
+    });
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -60,7 +77,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           {/* <div className="status">{ status }</div> */}
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
